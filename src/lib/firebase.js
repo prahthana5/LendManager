32a04ad436402b0fba9871407bf0a3b67aee14e7
Prabhakaran <prahthana5@gmail.com>
2025-12-26 23:22:44 +0530

    Firebase setup added

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your specific Firebase config
 const firebaseConfig = {
-    apiKey: "PLACEHOLDER_API_KEY",
-    authDomain: "PLACEHOLDER_AUTH_DOMAIN",
-    projectId: "PLACEHOLDER_PROJECT_ID",
-    storageBucket: "PLACEHOLDER_STORAGE_BUCKET",
-    messagingSenderId: "PLACEHOLDER_SENDER_ID",
-    appId: "PLACEHOLDER_APP_ID"
+    apiKey: "AIzaSyCb_IZoGuGlXcgG6XLaRF5Sfas6uUb4b7A",
+    authDomain: "lend-manager-7b5b3.firebaseapp.com",
+    projectId: "lend-manager-7b5b3",
+    storageBucket: "lend-manager-7b5b3.firebasestorage.app",
+    messagingSenderId: "708702544908",
+    appId: "1:708702544908:web:724aaf30e7e098147a7a29",
+    measurementId: "G-1RVWT4KR7H"
 };
 
 const app = initializeApp(firebaseConfig);
